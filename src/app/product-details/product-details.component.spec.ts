d7decc61af070e0fbbbe36d8b752e7123f60257a
martalalik <105340174+martalalik@users.noreply.github.com>
2022-11-08 12:55:21 +0100

    View product details

--- src/app/product-details/product-details.component.spec.ts
+++ src/app/product-details/product-details.component.spec.ts
@@ -8,9 +8,8 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductDetailsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
